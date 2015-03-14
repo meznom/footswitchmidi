ae6c9a3f2448d6ed2d157fe275a1c65414dfa3ac
Burkhard Ritter <burkhard@seite9.de>
2015-03-14 21:04:43 +0100

    Added command line options.
    * Command line options for help and debug messages.
    * Useful help / usage message.

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
  */
 
 #include <iostream>
+#include <string>
 #include "footswitch.hpp"
 #include "messagequeue.hpp"
 #include "jackmidi.hpp"
@@ -18,8 +19,60 @@ struct FootSwitchMessage
     bool on;
 };
 
+struct Options
+{
+    Options()
+    : debug{false}, help{false}
+    {}
+
+    bool debug;
+    bool help;
+};
+
+Options parseCommandLineArguments(int argc, char** argv)
+{
+    Options o;
+    for (int i=0; i<argc; i++)
+    {
+        std::string a = argv[i];
+        if (a=="--help" || a=="-h")
+        {
+            o.help = true;
+        }
+        else if (a=="--debug" || a=="-d")
+        {
+            o.debug = true;
+        }
+    }
+    return o;
+}
+
+void printUsageMessage ()
+{
+    std::cout
+    << "FootswitchMidi: Make the cheap PCsensor / DealExtreme foot switches " << std::endl
+    << "available via Jack Midi." << std::endl
+    << std::endl
+    << "Usage: footswitchmidi [options]" << std::endl
+    << std::endl
+    << "-d, --debug \tPrint debug messages" << std::endl
+    << "-h, --help  \tShow this help message" << std::endl
+    << std::endl
+    << "For footswitchmidi to work, you need to have the footswitch programmed with:" << std::endl
+    << "   $ ./footswitch -1 -k '<01>' -2 -k '<02>' -3 -k '<03>' " << std::endl
+    << "The footswitch program is available at https://github.com/rgerganov/footswitch." << std::endl;
+}
+
 int main (int argc, char** argv)
 {
+    Options o;
+    o = parseCommandLineArguments(argc, argv);
+    if (o.help)
+    {
+        printUsageMessage();
+        std::exit(EXIT_SUCCESS);
+    }
+
     MessageQueue<FootSwitchMessage, 100> queue;
     if (!queue.is_lock_free())
     {
@@ -32,7 +85,7 @@ int main (int argc, char** argv)
     jack.init();
 
     FootSwitch f;
-    f.debug_output = false;
+    f.debug_output = o.debug;
     f.handle_release = true;
     if (!f.init())
     {
@@ -45,7 +98,7 @@ int main (int argc, char** argv)
         if (!r)
             std::cerr << "Could not write message to queue --- queue is full" << std::endl;
     });
-    
+
     f.deinit();
 
     jack.deinit();
