38201876460f1a80bf2cd9b485a18d4530e539b2
Burkhard Ritter <burkhard@seite9.de>
2015-03-15 18:18:13 +0100

    Added simple OSC support. (Proper commit)
    * Send an OSC message for each footswitch button event.
    * Jack Midi and OSC can be enabled and disabled via command line
      options.
    * Set OSC destination host and port via command line options.
    * Invoke the program via `footswitchmidi` or `footswitchosc` (a symlink)
      to get Midi or OSC behaviour by default.
    * `make install` target that installs the program (and the symlink)

--- README.md
+++ README.md
@@ -1,47 +1,67 @@
-# FootswitchMidi
+# FootswitchMidi / FootswitchOSC
 
 ## Overview
 
 FootswitchMidi is a [Jack][] Midi client for those cheap foot switches sold by
-[DealExtreme][] and [PCsensor][], i.e. it translates switch action to Midi
-commands that can be used to control (Jack) Midi applications. The foot
-switches can be programmed with the [Footswitch command line tool][footswitch].
+[DealExtreme][] and [PCsensor][], i.e. it translates switch actions to Midi
+commands that can be used to control (Jack) Midi applications. FootswitchOSC
+translates switch actions to [OSC][] messages. The foot switches can be
+programmed with the [Footswitch command line tool][footswitch].
 
 ## Known limitations
 
-Right now the program is still in a very early and experimental stage, but
-basic functionality seems to be working.
+Right now the program is still in a very early stage, but basic functionality
+seems to be working.
 
 Some of the known limitations are:
 
 * Not robust with respect to plugging and unplugging
 * Only supports models with three switches
-* Footswitch needs to be programmed / prepared correctly
+* Footswitch needs to be programmed / prepared correctly: Program it with
+  `./footswitch -1 -k '<01>' -2 -k '<02>' -3 -k '<03>'`
 * Midi messages hardcoded for now
 * Midi messages not very accurate in time
+* Only one OSC destination. Does not support multiple listeners (i.e.
+  applications) via OSC.
+* Does not shutdown cleanly (i.e. deregister with libusb and Jack). This does
+  not seem to be a problem in practice.
 * With multiple pressed switches, a switch release might give the wrong switch
   (this is a hardware limitation)
 
 ## Building
 
-The program requires [Jack][] and [libusb-1.0][], as well as [cmake][] for
-building. On Ubuntu you can install them with:
+The program requires [Jack][], [libusb-1.0][] and [liblo][], as well as
+[cmake][] for building. On Ubuntu you can install them with:
 
 ```
-$ sudo apt-get install libjack-dev libusb-1.0-0-dev cmake
+$ sudo apt-get install libjack-dev libusb-1.0-0-dev liblo-dev cmake
 ```
 
-To download, build and run FootswitchMidi:
+To download, build, and install FootswitchMidi and FootswitchOSC to the home
+directory:
 
 ```
 $ git clone https://github.com/meznom/footswitchmidi.git
 $ cd footswitchmidi
 $ mkdir build
 $ cd build
-$ cmake ..
-$ make
-$ ./src/footswitchmidi
+$ cmake -DCMAKE_INSTALL_PREFIX=~ ..
+$ make install
 ```
+
+Run FootswitchMidi or FootswitchOSC:
+
+```
+# Requires that Jack is running
+$ footswitchmidi
+# Send OSC messages of the form "/footswitch [i:button] [i:on/off]" to
+# localhost, port 57120 (this is the default behaviour)
+$ footswitchosc --host 127.0.0.1 --port 57120
+# Get a useful help message
+$ footswitchmidi --help
+```
+
+To quit FootswichMidi or FootswitchOSC, simply press `Ctrl-C` (send `SIGTERM`).
 
 ## License
 
@@ -49,12 +69,14 @@ The code is distributed under the two-clause BSD license. Have a look at the
 `LICENSE` file for details.
 
 ---
-Burkhard Ritter (<burkhard@seite9.de>), September 2014
+Burkhard Ritter (<burkhard@seite9.de>), March 2015
 
 
 [Jack]: http://jackaudio.org/
-[DealExtreme]: http://www.dx.com/p/usb-triple-action-foot-switch-keyboard-control-foot-pedal-56508#.U_0L1db7sjg 
+[OSC]: http://opensoundcontrol.org/
+[DealExtreme]: http://www.dx.com/p/usb-triple-action-foot-switch-keyboard-control-foot-pedal-56508#.U_0L1db7sjg
 [PCsensor]: http://www.pcsensor.com/index.php?_a=category&cat_id=41
 [footswitch]: https://github.com/rgerganov/footswitch
-[libusb-1.0]: http://www.libusb.org/ 
+[libusb-1.0]: http://www.libusb.org/
+[liblo]: http://liblo.sourceforge.net/
 [cmake]: http://www.cmake.org/
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,6 @@
 /*
- * Copyright (c) 2014 Burkhard Ritter
+ * Copyright (c) 2014, 2015 Burkhard Ritter
  * This code is distributed under the two-clause BSD License.
- */
-/*
- * For footswitchmidi to work, we need to have the footswitch programmed with:
- * ./footswitch -1 -k '<01>' -2 -k '<02>' -3 -k '<03>'
  */
 
 #include <iostream>
@@ -12,6 +8,7 @@
 #include "footswitch.hpp"
 #include "messagequeue.hpp"
 #include "jackmidi.hpp"
+#include "osc.hpp"
 
 struct FootSwitchMessage
 {
@@ -21,18 +18,31 @@ struct FootSwitchMessage
 
 struct Options
 {
-    Options()
-    : debug{false}, help{false}
-    {}
-
-    bool debug;
-    bool help;
+    bool debug = false;
+    bool help = false;
+    bool osc = false;
+    bool midi = true;
+    std::string osc_host = "127.0.0.1";
+    std::string osc_port = "57120";
 };
 
 Options parseCommandLineArguments(int argc, char** argv)
 {
     Options o;
-    for (int i=0; i<argc; i++)
+
+    std::string name = argv[0];
+    if (name=="footswitchmidi")
+    {
+        o.midi = true;
+        o.osc = false;
+    }
+    else if (name=="footswitchosc")
+    {
+        o.midi = false;
+        o.osc = true;
+    }
+
+    for (int i=1; i<argc; i++)
     {
         std::string a = argv[i];
         if (a=="--help" || a=="-h")
@@ -43,6 +53,30 @@ Options parseCommandLineArguments(int argc, char** argv)
         {
             o.debug = true;
         }
+        else if (a=="--osc" || a=="-o")
+        {
+            o.osc = true;
+        }
+        else if (a=="--no-osc")
+        {
+            o.osc = false;
+        }
+        else if (a=="--midi" || a=="-m")
+        {
+            o.midi = true;
+        }
+        else if (a=="--no-midi")
+        {
+            o.midi = false;
+        }
+        else if (a=="--osc-host" && (i+1)<argc && std::string(argv[i+1])[0]!='-')
+        {
+            o.osc_host = std::string(argv[i+1]);
+        }
+        else if (a=="--osc-port" && (i+1)<argc && std::string(argv[i+1])[0]!='-')
+        {
+            o.osc_port = std::string(argv[i+1]);
+        }
     }
     return o;
 }
@@ -50,13 +84,20 @@ Options parseCommandLineArguments(int argc, char** argv)
 void printUsageMessage ()
 {
     std::cout
-    << "FootswitchMidi: Make the cheap PCsensor / DealExtreme foot switches " << std::endl
-    << "available via Jack Midi." << std::endl
+    << "FootswitchMidi / FootswitchOSC: Make the cheap PCsensor / DealExtreme " << std::endl
+    << "foot switches available via Jack Midi or OSC." << std::endl
     << std::endl
     << "Usage: footswitchmidi [options]" << std::endl
+    << "   or: footswitchosc [options]" << std::endl
     << std::endl
-    << "-d, --debug \tPrint debug messages" << std::endl
-    << "-h, --help  \tShow this help message" << std::endl
+    << "-h, --help     \tShow this help message" << std::endl
+    << "-d, --debug    \tPrint debug messages" << std::endl
+    << "-m, --midi     \tEnable Jack Midi (default when invoked as footswitchmidi)" << std::endl
+    << "    --no-midi  \tDisable Jack Midi" << std::endl
+    << "-o, --osc      \tEnable OSC (default when invoked as footswitchosc)" << std::endl
+    << "    --no-osc   \tDisable OSC" << std::endl
+    << "    --osc-host \tHost to send OSC messages to (default: 127.0.0.1)" << std::endl
+    << "    --osc-port \tPort to send OSC messages to (default: 57120)" << std::endl
     << std::endl
     << "For footswitchmidi to work, you need to have the footswitch programmed with:" << std::endl
     << "   $ ./footswitch -1 -k '<01>' -2 -k '<02>' -3 -k '<03>' " << std::endl
@@ -65,6 +106,9 @@ void printUsageMessage ()
 
 int main (int argc, char** argv)
 {
+    /*
+     * Command line options
+     */
     Options o;
     o = parseCommandLineArguments(argc, argv);
     if (o.help)
@@ -73,17 +117,9 @@ int main (int argc, char** argv)
         std::exit(EXIT_SUCCESS);
     }
 
-    MessageQueue<FootSwitchMessage, 100> queue;
-    if (!queue.is_lock_free())
-    {
-        std::cerr << "Our message queue is not lock free!" << std::endl;
-        std::exit(EXIT_FAILURE);
-    }
-
-    JackMidi<decltype(queue)> jack(queue);
-
-    jack.init();
-
+    /*
+     * Initialize footswitch
+     */
     FootSwitch f;
     f.debug_output = o.debug;
     f.handle_release = true;
@@ -92,14 +128,58 @@ int main (int argc, char** argv)
         std::cerr << "Could not initialize footswitch" << std::endl;
         std::exit(EXIT_FAILURE);
     }
+    std::cout << "Initialized footswitch" << std::endl;
 
-    f.run([&queue](unsigned char i, bool on) {
-        const bool r = queue.push(FootSwitchMessage{i, on});
-        if (!r)
-            std::cerr << "Could not write message to queue --- queue is full" << std::endl;
+    /*
+     * Initialize message queue and Jack Midi
+     */
+    MessageQueue<FootSwitchMessage, 100> queue;
+    if (!queue.is_lock_free())
+    {
+        std::cerr << "Our message queue is not lock free!" << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+
+    JackMidi<decltype(queue)> jack(queue);
+    if (o.midi)
+    {
+        if (!jack.init())
+        {
+            std::cerr << "Could not initialize Jack Midi" << std::endl;
+            std::exit(EXIT_FAILURE);
+        }
+        std::cout << "Initialized Jack Midi" << std::endl;
+    }
+
+    /*
+     * Initialize OSC
+     */
+     OSC osc{o.osc_host,o.osc_port};
+     if (o.osc)
+     {
+         std::cout << "Initialized OSC" << std::endl;
+     }
+
+    /*
+     * Run main event loop --- do something on every footswitch button event
+     */
+    f.run([&queue,&osc,&o](unsigned char i, bool on) {
+        if (o.midi)
+        {
+            const bool r = queue.push(FootSwitchMessage{i, on});
+            if (!r)
+                std::cerr << "Could not write message to queue --- queue is full" << std::endl;
+        }
+        if (o.osc)
+        {
+            osc.notify(FootSwitchMessage{i, on});
+        }
     });
 
+    /*
+     * Clean up
+     */
     f.deinit();
-
-    jack.deinit();
+    if (o.midi)
+        jack.deinit();
 }
